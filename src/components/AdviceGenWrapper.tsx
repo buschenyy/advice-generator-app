8dd99a69112c19a04d6e777a267d25b27485f8ad
buschenyy <buschenyy@gmail.com>
2023-04-14 20:26:05 +0200

    async await refactoring

--- src/components/AdviceGenWrapper.tsx
+++ src/components/AdviceGenWrapper.tsx
@@ -1,28 +1,26 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 const API = 'https://api.adviceslip.com/advice'
+const intialState = (await rest()).slip
+
+async function rest() {
+  const response = await fetch(API)
+  return await response.json()
+}
 
 const AdviceGenWrapper = () => {
-  const [advice, setAdvice] = useState<{ id: number; advice: string } | null>(
-    null
+  const [advice, setAdvice] = useState<{ id: number; advice: string }>(
+    intialState
   )
-
-  useEffect(() => rest(), [])
-
-  const rest = () => {
-    fetch(API)
-      .then((res) => res.json())
-      .then((data) => setAdvice(data.slip))
-  }
 
   return (
     <>
       {advice && (
         <div>
-          <div>{advice.id}</div>
-          <div>{advice.advice}</div>
+          <span>Advice #{advice.id}</span>
+          <p>{advice.advice}</p>
           <hr />
-          <button onClick={rest}></button>
+          <button onClick={async () => setAdvice((await rest()).slip)}></button>
         </div>
       )}
     </>
